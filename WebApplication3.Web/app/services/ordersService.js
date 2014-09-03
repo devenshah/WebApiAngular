fd4cd30f8c3f8aa8b277b3b9452a6cf2472be9d0
indiwiz <devenshah1974@yahoo.com>
2014-09-03 15:40:20 +0100

    Enable HTTPS on Web API

--- WebApplication3.Web/app/services/ordersService.js
+++ WebApplication3.Web/app/services/ordersService.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 app.factory('ordersService', ['$http', function ($http) {
 
-    var serviceBase = 'http://localhost:50238/';
+    var serviceBase = 'https://localhost:44300/'; /// https://localhost:44300/
     var ordersServiceFactory = {};
 
     var _getOrders = function () {
--- WebApplication3/App_Start/WebApiConfig.cs
+++ WebApplication3/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
+using WebApplication3.Filters;
 
 namespace WebApplication3
 {
@@ -13,6 +14,7 @@ namespace WebApplication3
         {
             // Web API configuration and services
             config.Filters.Add(new AuthorizeAttribute());
+            config.Filters.Add(new RequireHttpsAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
